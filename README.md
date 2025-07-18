d26b2e4e86d8b3ef056ccf5a18395e8698657070
sammyne <xiangmin-li@qq.com>
2025-07-18 01:06:56 +0000

    feat: demo hello-world

---
+++ README.md
@@ -0,0 +1,7 @@
+# libuv 库示例程序
+
+本仓库用于记录 libuv 库的示例程序，用于日常学习。
+
+## 参考文献
+- https://nikhilm.github.io/uvbook/
+- https://github.com/bmatcuk/libuv-rs/tree/master/examples
---
+++ crates/hello-world/src/main.rs
@@ -0,0 +1,18 @@
+use anyhow::Context;
+use libuv::prelude::*;
+use libuv::Loop;
+
+fn main() -> anyhow::Result<()> {
+    // Initialize the libuv loop
+    let mut l = Loop::new().context("new loop")?;
+
+    println!("Now quitting.");
+
+    let _ = l.run(RunMode::Default);
+
+    // Loop 的析构函数会调用 uv_loop_delete，底层会调用 close 函数。
+    // 显示调用 close 函数会导致析构时断言失败。
+    // l.close().context("close loop")?;
+
+    Ok(())
+}
